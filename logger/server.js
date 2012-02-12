8a6ca26495c64273eb1ec8a5cc7d3e05c99e7448
Aaron Stannard <aaron@aaronstannard.com>
2012-02-12 01:49:40 -0800

    Made fixes to template

--- logger/server.js
+++ logger/server.js
@@ -16,6 +16,7 @@ var app = module.exports = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.set('view options', { pretty: true });
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
